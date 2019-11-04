f0545e1f67d7056583c9b75db8658a7df3c6e052
Vlad Karpushin <karpushin@ngs.ru>
2019-11-04 15:17:30 +0700

    code: +++

--- src matlab/equalizer.m
+++ src matlab/equalizer.m
@@ -1,3 +1,22 @@
 % 2019-11-04
 % equalizer
-function equalizer()
+function [H] = equalizer(s_a, s_b, Fs)
+
+S_A_PSD = fft(s_a).*conj(fft(s_a));   %power spectrum density
+S_B_PSD = fft(s_b).*conj(fft(s_b));   %power spectrum density
+
+x = 1:length(s_a);
+x = x / length(x) * Fs;
+
+figure, plot(x, S_A_PSD);
+xlabel('Hz')
+title('PSD of sa');
+
+figure, plot(x, S_B_PSD);
+xlabel('Hz')
+title('PSD of sb');
+
+H = S_B_PSD ./ S_A_PSD;
+figure, plot(x, H);
+xlabel('Hz')
+title('H');
--- src matlab/main58_BPSK_12_PLL_9_one_pc_test_24_constell_11_filter_8.m
+++ src matlab/main58_BPSK_12_PLL_9_one_pc_test_24_constell_11_filter_8.m
@@ -271,12 +271,13 @@ disp(['StdSignSync = ',num2str(StdSignSync(i))]);
 [EstSignal_b MaxSignSync MinSignSync Err delta StdSignSync SignalContell indexA indexB] = CalcSignalEstimationNew4(CorrIntegral,threshold, SignBarkerLong, Samples, SignalComplex); %This function estimates information bits (information signal)
 
 % equalizer start()
-z_new = z(indexA-length(SignBarkerLong):indexA);
+z_new = z(indexA-length(SignBarkerLong):indexA-1);
 x = 0:F*Td:(kt*nTotalBits*2*pi)-(F*Td);
 s_b = SignBarkerLong.*sin(x(1:length(SignBarkerLong)))';
+
 figure, plot(z_new(1:200));
 figure, plot(s_b(1:200));
-equalizer();
+equalizer(s_b, z_new, Fs);
 % equalizer stop()
 
 
