d65653f8f4859bca05567b00136c0d0e6706b764
Vlad Karpushin <karpushin@ngs.ru>
2019-12-08 13:56:45 +0700

    added show_sign_para()

--- src matlab/main58_OOK_threshold_17_B1B2_12_constell_7_SNR_7_filter_6.m
+++ src matlab/main58_OOK_threshold_17_B1B2_12_constell_7_SNR_7_filter_6.m
@@ -64,17 +64,7 @@ n = fix(n_inf_bits / period);
 n_total_bits = delay + 2 * length(SignBarkerB1) + n * length(SignBarkerB2) + n_inf_bits; % total bits plus delay
 %n_total_bits = delay + 2*length(SignBarker) + n_inf_bits;
 
-t = kt * n_total_bits / F;   %common transmit time
-disp(['Sampling rate = ', num2str(Fs),' Hz']);
-disp(['Freq of signal1 = ', num2str(F),' Hz']);
-disp(['Duration of one symbol = ', num2str(1000 * kt / F),' ms']);
-disp(['BW = ', num2str(2 / (kt / F)),' Hz']);
-disp(['Number of total transmitted bits = ', num2str(n_total_bits),' [bits]']);
-disp(['Number of information transmitted bits = ', num2str(n_inf_bits),' [bits]']);
-disp(['Physical layer gross bitrate = ', num2str(F / kt),' [bits/s]']);
-disp(['Net bitrate = ', num2str(fix(n_inf_bits / t)),' [bits/s]']);
-disp(['Symbol rate = ', num2str(F / kt),' [Hz]']);
-disp(['common transmit time = ', num2str(t),' [s]']);
+show_sign_para(kt, F, Fs, n_total_bits, n_inf_bits);
 %carrier signal forming(stop)
 
 %modulation(start)
