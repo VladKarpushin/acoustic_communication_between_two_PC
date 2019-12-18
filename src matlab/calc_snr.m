a781e78b1155599e286a43acc59b7e3c28c3acd5
Vlad Karpushin <karpushin@ngs.ru>
2019-12-18 07:40:33 +0700

    +

--- src matlab/calc_snr.m
+++ src matlab/calc_snr.m
@@ -8,7 +8,7 @@ index_b = index_b + len_of_barker;
 if (index_a-4 > 1) && (index_b > 1) && (index_a < length(z)) && (index_b < length(z))  %snr estimation 
     s = std(z(index_a:index_b));
     n = std(z(1:index_a - 4));
-    snr_estimated = s / n;
+    snr_estimated = (s / n) - 1;
     disp(['snr estimated = ', num2str(round(snr_estimated)), ' [times]']);
     disp(['snr estimated = ', num2str(round(10 * log10(snr_estimated))), ' [dB]']);
 else
