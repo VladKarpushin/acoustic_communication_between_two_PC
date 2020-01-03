67dd08c84ad00c28cfb8ae60e64e957a8657ec7d
Vlad Karpushin <karpushin@ngs.ru>
2020-01-03 11:09:12 +0700

    ++

--- src matlab/equalizer_first.m
+++ src matlab/equalizer_first.m
@@ -1,9 +1,9 @@
 % 2019-12-01
 % equalizer input point
 
-function [sign_y_out] = equalizer_first(sign_x, sign_y, w, indexA)
+function [sign_y_out] = equalizer_first(sign_x, sign_y, w, index_a)
 
-sign_y_cut = sign_y(indexA-length(sign_x):indexA-1)';
+sign_y_cut = sign_y(index_a - length(sign_x):index_a - 1)';
 H = equalizer_second(sign_x, sign_y_cut, w, length(sign_y));
 sign_y_out = real(ifft(fft(sign_y) .* H)); % should be (H)
 sign_y_out = sign_y_out - mean(sign_y_out);
