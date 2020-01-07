fa0147ad6c51146ccf46de73359a5056e3252d05
Vlad Karpushin <karpushin@ngs.ru>
2020-01-07 11:51:23 +0700

    ++

--- src matlab/calc_snr.m
+++ src matlab/calc_snr.m
@@ -3,13 +3,13 @@
 % 2020-01-02
 % took into account pll block
 
-function [snr_estimated] = calc_snr(z, len_of_barker, index_a, index_b, freq_burst_size)
+function [snr_estimated] = calc_snr(z, ind_a, ind_b, synch_burst_size, freq_burst_size)
 
-index_a = index_a - len_of_barker;
-index_b = index_b + len_of_barker;
-index_noise = index_a - 7 - freq_burst_size;
-if (index_noise > 1) && (index_b > 1) && (index_a < length(z)) && (index_b <= length(z))  %snr estimation 
-    s = std(z(index_a:index_b));
+ind_a = ind_a - synch_burst_size;
+ind_b = ind_b + synch_burst_size;
+index_noise = ind_a - 7 - freq_burst_size;
+if (index_noise > 1) && (ind_b > 1) && (ind_a < length(z)) && (ind_b <= length(z))  %snr estimation 
+    s = std(z(ind_a:ind_b));
     n = std(z(1:index_noise));
     snr_estimated = (s / n) - 1; % maybe "snr_estimated = (s / n);" is correct
     disp(['snr estimated = ', num2str(round(snr_estimated)), ' [times]']);
