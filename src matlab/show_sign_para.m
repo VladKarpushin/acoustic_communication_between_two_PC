1b09f662f010fd57d86ec11b3b48472fbee80842
Vlad Karpushin <karpushin@ngs.ru>
2020-01-12 12:17:07 +0700

    improved show_sign_para()

--- src matlab/show_sign_para.m
+++ src matlab/show_sign_para.m
@@ -5,7 +5,7 @@ function show_sign_para(kt, F, Fs, n_total_bits, n_inf_bits, delay)
 
 t = kt * (n_total_bits - delay) / F;   %common transmit time
 disp(['Sampling rate = ', num2str(Fs),' Hz']);
-disp(['Freq of signal = ', num2str(F),' Hz']);
+disp(['f0  = freq of signal = ', num2str(F),' Hz']);
 disp(['Duration of one symbol = ', num2str(1000 * kt / F),' ms']);
 disp(['BW = ', num2str(2 / (kt / F)),' Hz']);
 disp(['Number of total transmitted bits = ', num2str(n_total_bits - delay),' [bits]']);
@@ -14,3 +14,4 @@ disp(['Physical layer gross bitrate = ', num2str(F / kt),' [bits / s]']);
 disp(['Net bitrate = ', num2str(fix(n_inf_bits / t)),' [bits / s]']);
 disp(['Symbol rate = baud rate = ', num2str(F / kt),' [Hz]']);
 disp(['common transmit time = ', num2str(t),' [s]']);
+disp(['K = samples per one symbol = ', num2str(kt * Fs / F),' [samples]']);
