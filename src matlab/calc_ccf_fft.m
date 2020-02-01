ff4b2cb085f21f791abecb524046719b7ae6088f
Vlad Karpushin <karpushin@ngs.ru>
2020-02-01 09:49:41 +0700

    code: improved calc_ccf_fft

--- src matlab/calc_ccf_fft.m
+++ src matlab/calc_ccf_fft.m
@@ -19,23 +19,34 @@ if (length(sign_a) < n_border) | (length(sign_b) < n_border)
 end
 
 % определяем сигнал меньшей размерности (н)	
-sign1 = sign_b;
-sign2 = sign_a; % сигнал наименьшей размерности
-if length(sign_a) >= length(sign_b)
-    sign1 = sign_a;
-    sign2 = sign_b;	% сигнал наименьшей размерности
+% sign1 = sign_b;
+% sign2 = sign_a; % сигнал наименьшей размерности
+% if length(sign_a) >= length(sign_b)
+%     sign1 = sign_a;
+%     sign2 = sign_b;	% сигнал наименьшей размерности
+% end
+
+if length(sign_b) > length(sign_a)
+    tmp = sign_a;
+    sign_a = sign_b;    % long signal
+    sign_b = tmp;       % short signal
 end
 % определяем сигнал меньшей размерности (к)
 
 % делаем сигналы равной размерности (н)
-sign_tmp = sign2;
-sign2 = zeros(length(sign1), 1);
-sign2(1:length(sign_tmp)) = sign_tmp;
+% tmp = sign2;
+% sign2 = zeros(length(sign1), 1);
+% sign2(1:length(tmp)) = tmp;
+tmp = sign_b;
+%sign_b = zeros(length(sign_a), 1);
+sign_b = zeros(size(sign_a));
+sign_b(1:length(tmp)) = tmp;
+
 % делаем сигналы равной размерности (к)
 
-sign1_fft = fft(sign1);
-sign2_fft = fft(sign2);
-ccf = real(ifft(sign1_fft .* conj(sign2_fft)));
-den = max(sign_a) * max(sign_b) * length(sign_b);
+sign_a_fft = fft(sign_a);
+sign_b_fft = fft(sign_b);
+ccf = real(ifft(sign_a_fft .* conj(sign_b_fft)));
+den = max(sign_a) * max(sign_b) * length(tmp); % denominator
 ccf = ccf / den;
 end
