b885941b4b502266001692c4e8ba6faf116287cb
Vlad Karpushin <karpushin@ngs.ru>
2020-02-09 13:49:38 +0700

    code: ++

--- src matlab/show_sign_para.m
+++ src matlab/show_sign_para.m
@@ -10,7 +10,7 @@ disp(['Duration of one symbol = ', num2str(1000 * kt / F),' ms']);
 disp(['BW = ', num2str(2 / (kt / F)),' Hz']);
 disp(['Number of total transmitted bits = ', num2str(n_total_bits - delay),' [bits]']);
 disp(['Number of information transmitted bits = ', num2str(n_inf_bits),' [bits]']);
-disp(['Physical layer gross bitrate = ', num2str(F / kt),' [bits / s]']);
+disp(['Physical layer gross bitrate = ', num2str(F / kt),' [bits / s]']); % is not correct for qpsk
 disp(['Net bitrate = ', num2str(fix(n_inf_bits / t)),' [bits / s]']);
 disp(['Symbol rate = baud rate = ', num2str(F / kt),' [Hz]']);
 disp(['common transmit time = ', num2str(t),' [s]']);
