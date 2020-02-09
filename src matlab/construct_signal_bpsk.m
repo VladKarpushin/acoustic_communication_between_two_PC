4e388d37f09ab468e039a02a6a6e66ea1885703f
Vlad Karpushin <karpushin@ngs.ru>
2020-02-09 13:59:54 +0700

    code: ++

--- src matlab/construct_signal_bpsk.m
+++ src matlab/construct_signal_bpsk.m
@@ -1,6 +1,6 @@
-% this function constructs signal for bps transmission
+% this function constructs signal for qpsk transmission (q part)
 
-function [sign_out] = construct_signal_bpsk(sign_inf, sign_sync_b1, sign_sync_b2, delay, freq_burst_size)
+function [sign_out] = construct_signal_qpsk_q(sign_inf, sign_sync_b1, sign_sync_b2, delay, freq_burst_size)
 % input:
 % sign_inf   - information signal
 % sign_sync  - sync signal B1
@@ -12,8 +12,8 @@ function [sign_out] = construct_signal_bpsk(sign_inf, sign_sync_b1, sign_sync_b2
 
 n_total_bits = delay + freq_burst_size + length(sign_sync_b1) + length(sign_sync_b2) + length(sign_inf);
 sign_out = zeros(n_total_bits, 1);
-sign_out(1 + delay + freq_burst_size:delay + freq_burst_size + length(sign_sync_b1)) = sign_sync_b1;
-sign_out(1 + delay:delay + freq_burst_size) = 1;
-sign_out(n_total_bits - length(sign_sync_b2) + 1:n_total_bits) = sign_sync_b2;
+sign_out(1 + delay + freq_burst_size:delay + freq_burst_size + length(sign_sync_b1)) = 0;
+sign_out(1 + delay:delay + freq_burst_size) = 0;
+sign_out(n_total_bits - length(sign_sync_b2) + 1:n_total_bits) = 0;
 sign_out(1 + delay + freq_burst_size + length(sign_sync_b1):delay + freq_burst_size + length(sign_sync_b1) + length(sign_inf)) = sign_inf;
 end
