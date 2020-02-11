f10489cbe2c03d9f1442093cc66ce395bf0bb160
Vlad Karpushin <karpushin@ngs.ru>
2020-02-11 08:28:41 +0700

    code: added nargin

--- src matlab/calc_signal_estimation_bpsk.m
+++ src matlab/calc_signal_estimation_bpsk.m
@@ -4,7 +4,7 @@
 %for BPSK only
 %2016-11-05 added code for signal constellation
 %2016-12-20 added iAB1 and iBB1 export for SNR calculation
-function [est_signal_b, max_sync_b1, max_sync_b2, err, signal_constel, ind_a, ind_b] = calc_signal_estimation_bpsk(threshold, sign_barker_b1_long, sign_barker_b2_long, samples, signal_complex)
+function [est_signal_b, max_sync_b1, max_sync_b2, err, signal_constel, ind_a, ind_b] = calc_signal_estimation_bpsk(threshold, sign_barker_b1_long, sign_barker_b2_long, samples, signal_complex, m_type)
 % input:
 % 	corr_integral    - cross correlation function (CCF) received signal SignR and sin wave. Another name is correlation integral
 % 	threshold       - resolver threshold. Should be zero for BPSK
@@ -22,6 +22,10 @@ function [est_signal_b, max_sync_b1, max_sync_b2, err, signal_constel, ind_a, in
 %   ind_a            - index of first symbol. For SNR estimation
 %   ind_b            - index of last symbol. For SNR estimation
 
+if nargin < 6
+    m_type = 'bpsk'; 
+end
+
 err = 0;
 max_sync_b1 = 0;
 max_sync_b2 = 0;
@@ -33,7 +37,6 @@ ind_b = 0;
 %corr_integral = real(signal_complex);
 est_signal_long = zeros(length(signal_complex), 1);
 est_signal_long = (2 * (real(signal_complex) > threshold)) - 1;    %resolver
-est_signal_long_q = (2 * (imag(signal_complex) > threshold)) - 1;    % resolver for q part of qpsk
 
 %****syncronization start*******
 sync_b1 = calc_ccf_fft(est_signal_long, sign_barker_b1_long, 0);
@@ -51,10 +54,13 @@ ind_a = ind_max_sync_b1 + length(sign_barker_b1_long);
 ind_b = ind_max_sync_b2 - 1;
 
 est_signal_b = long_to_short(est_signal_long(ind_a:ind_b), samples);
-est_signal_b_q = long_to_short(est_signal_long_q(ind_a:ind_b), samples);
 signal_constel = long_to_short(signal_complex(ind_a:ind_b), samples);
 %****syncronization stop*******
 
+if m_type == 'qpsk'
+    est_signal_long_q = (2 * (imag(signal_complex) > threshold)) - 1;    % resolver for q part of qpsk
+    est_signal_b_q = long_to_short(est_signal_long_q(ind_a:ind_b), samples);
+end
 
 if abs(length(est_signal_b) / 8 - fix(length(est_signal_b) / 8)) > 0                  %checking if length of est_signal_b is multiple with 8
     disp(['Error. abs(length(est_signal_b)/8 - fix(length(est_signal_b)/8)) > 0. length(est_signal_b) = ', num2str(length(est_signal_b))]);
