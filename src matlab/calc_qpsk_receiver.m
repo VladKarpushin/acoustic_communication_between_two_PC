6023bf7e7e77be1f4873a98455ba7ba055b1cdd8
Vlad Karpushin <karpushin@ngs.ru>
2020-02-14 21:42:20 +0700

    code: ++

---
+++ src matlab/calc_qpsk_receiver.m
@@ -0,0 +1,76 @@
+% 2019-12-15
+% coherent reception, information signal estimation, BER calculation
+
+function [est_signal_b, ind_a, ind_b] = calc_qpsk_receiver(z, samples, F, Fs, sign_barker_b1_long, sign_barker_b2_long, n_inf_bits, signal_inf_bits, criterion)
+
+%*******PLL start ******
+n = 50;
+%phi = pi * (0:n) / n;
+phi = linspace(0, pi, n);
+max_abs_corr_integral   = -2 * ones(n, 1);   % max(CCF received signal and sin wave), max(correlation integral)
+BER                     = -2 * ones(n, 1);   % BER is bit error rate
+max_sync_b1             = -2 * ones(n, 1);   % max(CCF of b1)
+max_sync_b2             = -2 * ones(n, 1);   % max(CCF of b2)
+delta                   = -2 * ones(n, 1);   %delta is difference between index(min_sync_b2) and index(max_sync_b1)
+threshold = 0;                      %resolver threshold. Should be zero for BPSK
+
+%ind_a                   = -2 * ones(n, 1);   %delta is difference between index(min_sync_b2) and index(max_sync_b1)
+
+for i = 1:n
+    [signal_complex] = calc_signal_complex(z, samples, F, Fs, phi(i));   %coherent reception
+    [est_signal_b, max_sync_b1(i), max_sync_b2(i), ~, ~, ind_a, ind_b] = calc_signal_estimation_bpsk(threshold, sign_barker_b1_long, sign_barker_b2_long, samples, signal_complex, 'qpsk'); %This function estimates information bits (information signal)
+    delta(i) = ind_b - ind_a;
+    max_abs_corr_integral(i) = max(abs(real(signal_complex)));
+    BER(i) = calc_ber(signal_inf_bits, est_signal_b, n_inf_bits, 'debug');
+    %BER(i) = calc_ber(signal_inf_bits, est_signal_b, n_inf_bits);
+end
+err_syst = n_inf_bits * samples - delta; %systematic error between n_inf_bits*samples and delta
+%PLL_offset_vs_BER = [PLL_offset_n' max_abs_corr_integral max_sync_b1 min_sync_b2 max_sync_b1-min_sync_b2 BER delta err_syst std_sign_sync];
+%*******PLL stop ******
+
+%*******output result (start)*********
+m = -2;
+i = -2;
+disp(['phi precision = ', num2str(180 / n), ' [degrees]']);
+
+if criterion == 'c1'
+    [m i] = max(max_sync_b1 + max_sync_b2);   %PLL criterion1: max(CCF_b1) + max(CCF_b2) = max
+    disp(['PLL criterion1: max(max_sync_b1 + max_sync_b2)']);
+    disp(['phi c1 = ', num2str(phi(i)), ' [radians]']);
+    disp(['phi c1 = ', num2str(phi(i) * 180 / pi), ' [degrees]']);
+    disp(['BER c1 = ', num2str(BER(i))]);
+elseif criterion == 'c2'
+    [m i] = max(max_sync_b1);   %PLL criterion2: max(max_sync_b1)
+    disp(['PLL criterion2: max(max_sync_b1)']);
+    disp(['phi c2 = ', num2str(phi(i)), ' [radians]']);
+    disp(['phi c2 = ', num2str(phi(i) * 180 / pi), ' [degrees]']);
+    disp(['BER c2 = ', num2str(BER(i))]);
+else
+	disp(['Calc_bpsk_receiver. Wrong criterion']);
+end
+
+if abs(err_syst(i)) > samples / 2      
+    [m i] = min(abs(err_syst));          %PLL criterion3: systematic error = min
+    disp(['PLL criterion3: systematic error = min']);
+    disp(['phi c3 = ', num2str(phi(i)), ' [radians]']);
+    disp(['phi c3 = ', num2str(phi(i) * 180 / pi), ' [degrees]']);
+    disp(['BER c3 = ', num2str(BER(i))]);
+end
+
+[signal_complex] = calc_signal_complex(z, samples, F, Fs, phi(i));   %coherent reception
+[est_signal_b, ~, ~, ~, signal_constel, ind_a, ind_b] = calc_signal_estimation_bpsk(threshold, sign_barker_b1_long, sign_barker_b2_long, samples, signal_complex, 'qpsk'); %This function estimates information bits (information signal)
+
+plot_time(real(signal_complex), Fs, 'sec', 'corr integral')
+
+x = 1:length(z);
+x = x / Fs;
+figure, plot(x, real(signal_complex), 'r', x, z, 'b');
+xlabel('sec');
+title('corr-integral (r) and z (b)');
+
+c = linspace(1, 10, length(signal_constel));                         % from black to yellow
+figure, scatter(real(signal_constel), imag(signal_constel), [], c);     % create a scatter plot and vary the circle color.
+axis equal; %Use the same length for the data units along each axis.
+xlabel('In Phase');
+ylabel('Quadrature');
+title('Signal Constellation');
---
+++ src matlab/main_QPSK.m
@@ -0,0 +1,186 @@
+%2016-01-12
+% Sound transfer/receiver
+% 2016-03-10 zero errors during signal transmittion
+% 2016-04-04 transferred file from the same pc
+% 2016-04-26 finished
+% 2016-05-22 added duration of one symbol output/output
+% 2016-05-29 added time delay in a beginning of transmission (unit is bit)
+% 2016-06-05 added comments - samples
+% 2016-06-18 auto-calculated threshold for OOK,
+% 2016-06-19 released BPSK with BER=0
+% 2016-07-03 started to develop "phase locking" (PLL - Phase Locked Loop)
+% 2016-07-06 realised PLL algorithm, tested for models
+% 2016-07-09 Successfully tested PLL with real signals 
+% 2016-07-22 Met problem with sync during >2KB transmitting
+% 2016-07-24 std(VKF) output, improved PLL criteria
+% 2016-07-27 added comments
+% 2016-08-13 added GetPeriodicBarkerCode, InsertSyncB2 and InsertSyncB1
+% 2016-08-21 added sign_barker_long = Short2Long(sign_barker, samples);
+% 2016-08-25 added Long2Short
+% 2016-11-05 added code for signal constellation
+% 2016-11-06 added color for phase
+% 2016-11-20 updated modulation code
+% 2016-11-30 improved PSD code
+% 2016-12-13 added tx filter code
+% 2016-12-20 added code for SNR estimation
+% 2017-01-02 added spectrogram
+% 2019-11-04 start to equalizer development
+% 2020-01-05 added freq correction
+
+close all, clc, clear all;
+
+%read a file start
+%[signal_inf_bits errmsg] = file2signal('input\main13.m');
+%[signal_inf_bits, errmsg] = file2signal('..\input\main13_2KB.m');
+filename = 'ones_1KB.m';
+[signal_inf_bits, errmsg] = file2signal(strcat('..\input\', filename));
+
+if ~isempty(errmsg)
+    disp('file2signal error');
+    disp(errmsg);
+    return;
+end
+%read a file stop
+
+%******************************
+%*******Transmitter************
+%******************************
+%Fs - Sampling rate in Hz. Valid values depend on the specific audio hardware installed. Typical values supported by most sound cards are 8000, 11025, 22050, 44100, 48000, and 96000 Hz.
+%Fs = 96000;
+Fs = 11025;     %sample rate
+F = Fs / 3;  %frequency of signal, 200<F<Fs/2, [Hz]. F = Fs/14 - max, F = Fs/30 - max for Fs = 96000; For example, F = Fs/30, 30 - number of samples per one wave
+%F = Fs/5;  %frequency of signal, 200<F<Fs/2, [Hz]. F = Fs/14 - max, F = Fs/30 - max for Fs = 96000; For example, F = Fs/30, 30 - number of samples per one wave
+kt = 2;     %coefficient of duration of one symbol, kt/F = duration of one symbol
+n_inf_bits = 1024 * 4 * 1;      % number of information bits
+n_inf_bits2 = n_inf_bits / 2;
+%n_inf_bits = 1024 * 4 * 1;      % number of information bits
+%n_inf_bits = length(signal_inf_bits);
+Td = 2 * pi / Fs;   % sampling interval
+delay = 1000;       % time delay in a beginning of transmission, unit is bits
+freq_burst_size = 1000; % frequency correction burst size, unit is bits
+
+%*****Barker codes set generation (start)*****
+n_sign_barker = 75;   % quantity of Barker codes in a set.
+sign_barker_one_b1 = [1 1 1 1 1 -1 -1 1 1 -1 1 -1 1]'; % Barker code N=13. Barker codes, which are subsets of PN sequences, are commonly used for frame synchronization in digital communication systems. Barker codes have length at most 13 and have low correlation sidelobes
+sign_barker_one_b2 = [1 1 1 -1 -1 -1 1 -1 -1 1 -1]'; % Barker code N=11. Barker codes, which are subsets of PN sequences, are commonly used for frame synchronization in digital communication systems.
+sign_barker_b1 = get_periodic_barker_code(sign_barker_one_b1, n_sign_barker);
+sign_barker_b2 = get_periodic_barker_code(sign_barker_one_b2, n_sign_barker);
+%*****Barker codes set generation (stop)*****
+
+n_total_bits_i = delay + freq_burst_size + length(sign_barker_b1) + length(sign_barker_b2) + n_inf_bits2;
+n_total_bits_q = n_inf_bits2;
+show_sign_para(kt, F, Fs, n_total_bits_i, n_inf_bits, delay);
+
+%modulation(start)
+signal_inf_bits = 2 * randi([0, 1], n_inf_bits, 1) - 1; % model of information signal is noise
+signal_inf_bits_i = signal_inf_bits(1: n_inf_bits2);
+signal_inf_bits_q = signal_inf_bits(1 + n_inf_bits2: end);
+
+% signal_inf_bits = -1*ones(n_inf_bits,1);       %information signal = [1 -1 1 -1 1 -1]
+% n=1:2:n_inf_bits;
+% signal_inf_bits(n) = 1;
+% 
+% signal_inf_bits = ones(n_inf_bits,1);       %information signal = [-1 -1 -1 1 1 1]
+% signal_inf_bits(1:fix(n_inf_bits/2)) = -1;
+
+%adding sync marks (start)
+signal_i  = construct_signal_bpsk(signal_inf_bits_i, sign_barker_b1, sign_barker_b2, delay, freq_burst_size);
+signal_q  = construct_signal_bpsk(signal_inf_bits_q, zeros(length(sign_barker_b1), 1), zeros(length(sign_barker_b2), 1), delay, freq_burst_size);
+%adding sync marks (stop)
+
+samples = kt * Fs / F;       %!!!! number of samples per one symbol
+if abs(samples - fix(samples)) > 0                  % check Freq assignment error
+    disp(['Error. Freq assignment error. samples = kt*Fs/F = ', num2str(samples)]);
+    return;
+end
+
+x = 0:F * Td:(kt * n_total_bits_i * 2 * pi) - (F * Td);
+signal_long_i = short_to_long(signal_i, samples);        % BPSK
+signal_long_i = shaper_filter(signal_long_i, samples, Fs);     %filtering
+
+signal_long_q = short_to_long(signal_q, samples);        % QPSK q part
+signal_long_q = shaper_filter(signal_long_q, samples, Fs);     %filtering
+
+u = signal_long_i .* cos(x)' + signal_long_q .* sin(x)';
+%u = signal_long_i .* cos(x)';                                 % u[n] can be replaced by s[n]
+u = u / std(u);
+%modulation(stop)
+
+%air channel modeling (start)
+% SNR = 10;        %signal to noise ratio
+% signal_noise = randn(length(u), 1) / SNR;
+% u = u + signal_noise;
+%air channel modeling (stop)
+
+x1 = 0:2 * pi / 100:kt * 2 * pi;
+x2 = 0:F * Td:kt * 2 * pi;
+figure, plot(x1, sin(x1), x2, sin(x2), 'o');
+title('one symbol');
+
+plot_time(u, Fs, 'sec', 'transmitted signal u')
+plot_psd(u, Fs, 'Hz', 'PSD of transmitted signal u');
+
+figure, spectrogram(u, 400, 100, [], Fs);
+title('Transmitted signal spectrogram');
+
+nBits = 24;
+%sound(u, Fs, nBits);         %modulated signal
+
+%**************************************************
+%***************Receiver***************************
+%**************************************************
+
+%Fs = 44100;% 44100; %96000 
+% F = 5*Fs/100;  %frequency of signal, 200<F<Fs/2, [Hz]. even(1*Fs/100, 2*Fs/100, 4*Fs/100). F = 2(and 4)*Fs/100 - optimum
+% kF = 4;         %f1 = kF*f0, kF=2(and 8) - optimum
+% n_inf_bits = 1*8*1024;   %number of information bits
+
+tt = 3 + kt * (freq_burst_size + length(sign_barker_b1) + length(sign_barker_b2) + n_inf_bits2) / F;   %common transmit time
+nBits = 24;
+samples = kt * Fs / F;       %!!!! number of samples per one symbol
+if abs(samples - fix(samples)) > 0                  %check Freq assignment error
+    disp(['Error. Freq assignment error. samples = kt*Fs/F = ', num2str(samples)]);
+    return;
+end
+
+recObj = audiorecorder(Fs, nBits, 1);
+get(recObj);
+% Record your voice for 5 seconds.
+%recObj = audiorecorder;
+disp('Start recording.');
+recordblocking(recObj, tt);
+disp('End of Recording.');
+
+% Store data in double-precision array.
+z = getaudiodata(recObj)';      %received signal
+z = u';
+
+sign_barker_b1_long = short_to_long(sign_barker_b1, samples);
+sign_barker_b2_long = short_to_long(sign_barker_b2, samples);
+
+plot_time(z, Fs, 'sec', 'recorded signal z')
+plot_psd(z, Fs, 'Hz', 'PSD of received signal z');
+
+figure, spectrogram(z, 400, 100, [], Fs); % Compute the short-time Fourier transform. Divide the waveform into 400-sample segments with 100-sample overlap
+title('Received signal spectrogram');
+
+%Fs = Fs - Fs * 7 * 10^-6; % actual frequency drift is about 0.12 Hz
+[~, ind_a, ~] = calc_qpsk_receiver(z, samples, F, Fs, sign_barker_b1_long, sign_barker_b2_long, n_inf_bits2, signal_inf_bits, 'c2');
+
+[~, Fs_new] = freq_correction(z, ind_a, length(sign_barker_b1_long), freq_burst_size * samples, Fs, F);
+[~, ind_a, ~] = calc_qpsk_receiver(z, samples, F, Fs_new, sign_barker_b1_long, sign_barker_b2_long, n_inf_bits2, signal_inf_bits, 'c2');
+
+z_new = equalizer(z, ind_a, sign_barker_b1_long, 12 * n_sign_barker, samples, Fs_new, F);
+[est_signal_b, ind_a, ind_b] = calc_qpsk_receiver(z_new, samples, F, Fs_new, sign_barker_b1_long, sign_barker_b2_long, n_inf_bits2, signal_inf_bits, 'c1');
+
+calc_snr(z, ind_a - length(sign_barker_b1_long), ind_b + length(sign_barker_b2_long), freq_burst_size * samples);
+
+%write file (start)
+[errmsg] = signal2file('output\output.txt', est_signal_b);
+%[errmsg] = signal2file(strcat('output\',char(datetime),'.txt'), est_signal_b);
+if ~isempty(errmsg)
+    disp('signal2file error');
+    disp(errmsg);
+    return;
+end
+%write file (stop)
