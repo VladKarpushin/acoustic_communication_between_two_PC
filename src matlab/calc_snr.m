dddfd2d5dd2d63cfee1c4ca4fe22dfff72903c5f
Vlad Karpushin <karpushin@ngs.ru>
2020-03-01 12:42:34 +0700

    code: improved calc_snr()

--- src matlab/calc_snr.m
+++ src matlab/calc_snr.m
@@ -10,8 +10,10 @@ if (index_noise > 1) && (ind_b > 1) && (ind_a < length(z)) && (ind_b <= length(z
     s = std(z(ind_a:ind_b));
     n = std(z(1:index_noise));
     snr_estimated = (s / n) - 1; % maybe "snr_estimated = (s / n);" is correct
-    disp(['snr estimated = ', num2str(round(snr_estimated)), ' [times]']);
-    disp(['snr estimated = ', num2str(round(10 * log10(snr_estimated))), ' [dB]']);
+    disp(['snr estimated (std) = ', num2str(round(snr_estimated)), ' [times]']);
+    disp(['snr estimated (std)= ', num2str(round(10 * log10(snr_estimated))), ' [dB]']);
+    disp(['snr estimated (var) = ', num2str(round(snr_estimated ^ 2)), ' [times]']);
+    %disp(['snr estimated (var)= ', num2str(round(10 * log10(snr_estimated ^ 2))), ' [dB]']); % maybe correct is 20 * log
 else
     disp('snr estimation error');
 end
